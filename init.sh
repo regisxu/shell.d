b95da36a7e5554fb304715b06ffcea3e33b953e8
Regis Xu <ruizxu@stubhub.com>
2021-07-29 21:54:33 +0800

    Add istioctl and kubectl.

--- init.sh
+++ init.sh
@@ -14,6 +14,12 @@ export LESS="-XRF"
 
 source ~/.shell/git-prompt.sh
 
+source ~/.shell/_kubectl
+alias k=kubectl
+complete -F __start_kubectl k
+
+source ~/.shell/_istioctl
+
 PROMPT='%{$fg[green]%}%n@%m%{$reset_color%} %{$fg[yellow]%}%~%{$fg[cyan]%}$(__git_ps1)%{$reset_color%}'$'\n'"$ "
 
 #export NVM_DIR="/c/regis/.nvm"
@@ -22,15 +28,16 @@ PROMPT='%{$fg[green]%}%n@%m%{$reset_color%} %{$fg[yellow]%}%~%{$fg[cyan]%}$(__gi
 export JAVA_DEBUG="-agentlib:jdwp=transport=dt_socket,suspend=y,server=y,address=0.0.0.0:8787"
 
 export GIT_HOME=/c/regis/apps/PortableGit
-export JAVA_HOME=/c/regis/dev/jdk1.8.0_131
+export JAVA_HOME=/c/regis/dev/jdk-11.0.1
 export MAVEN_HOME=/c/regis/dev/apache-maven-3.5.0
 export GRADLE_HOME=/c/regis/dev/gradle-4.10
 export NODE_HOME="/c/Program Files/nodejs"
 export PYTHON_HOME=/c/regis/dev/python-3.7.2
+export AZURE_HOME="/c/Program Files (x86)/Microsoft SDKs/Azure/CLI2"
 
 export MAVEN_OPTS="$MAVEN_OPTS -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true"
 
-export PATH=$GIT_HOME/bin:"$NODE_HOME":$PYTHON_HOME:$PYTHON_HOME/Scripts:~/bin:~/apps/emacs/bin:$JAVA_HOME/jre/bin:$JAVA_HOME/bin:$ANT_HOME/bin:$MAVEN_HOME/bin:$GRADLE_HOME/bin:$PATH:/mingw64/bin:/mingw32/bin:"/c/Program Files/Docker/Docker/resources/bin":/c/Users/ruizxu/AppData/Roaming/npm
+export PATH=$GIT_HOME/bin:"$NODE_HOME":$PYTHON_HOME:$PYTHON_HOME/Scripts:~/bin:~/apps/emacs/bin:$JAVA_HOME/jre/bin:$JAVA_HOME/bin:$ANT_HOME/bin:$MAVEN_HOME/bin:$GRADLE_HOME/bin:$PATH:/mingw64/bin:/mingw32/bin:"/c/Program Files/Docker/Docker/resources/bin":/c/Users/ruizxu/AppData/Roaming/npm:"$AZURE_HOME/wbin"
 
 export STUBPROXY=http://10.131.71.31:80
 
